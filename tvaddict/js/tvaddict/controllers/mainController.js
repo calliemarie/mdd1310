3056b77849cedaa789e86950371480227da74c8c
Callie Kavourgias <calliemariek@gmail.com>
2013-10-16 14:33:10 -0400

    Fixed errors

--- tvaddict/js/tvaddict/controllers/mainController.js
+++ tvaddict/js/tvaddict/controllers/mainController.js
@@ -1,16 +1,16 @@
 app.controller("mainController", function($scope, $http){
- 
-    $scope.apiKey = "[2570b1bc1a12b222f700fc67b507b8bf]";
+    $scope.apiKey = "2570b1bc1a12b222f700fc67b507b8bf";
     $scope.results = [];
     $scope.init = function() {
-        //creates requires a start date
+        //API requires a start date
         var today = new Date();
         //Create the date string and ensure leading zeros if required
         var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
-         
+            //As we are getting our data from an external source, we need to format the data so we can use it to our desired effect
+            //For each day, get all the episodes
             angular.forEach(data, function(value, index){
-              
+                //The API stores the full date separately from each episode. Save it so we can use it later
                 var date = value.date;
                 //For each episodes, add it to the results array
                 angular.forEach(value.episodes, function(tvshow, index){
